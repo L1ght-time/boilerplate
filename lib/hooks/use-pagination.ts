29a4316d9c5adcc7c71ab779f7e5eb2269ffc02d
L1ght-time <light.time.1995@gmail.com>
2025-10-21 20:55:59 +0300

    fix: usePagination hook

--- lib/hooks/use-pagination.ts
+++ lib/hooks/use-pagination.ts
@@ -2,41 +2,41 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useSearchParamsActions } from "~/lib/hooks/use-search-params-actions";
 
+const pageSize = 10;
+
 export const usePagination = <T>(data: T[]) => {
   const searchParams = useSearchParams();
   const { set } = useSearchParamsActions();
-  const pageIndex = Number(searchParams.get("page")) || 1;
-  const normalizedToMinPageIndex = Math.max(pageIndex - 1, 0);
+
+  const pageIndexFromUrl = Number(searchParams.get("page")) || 1;
+  const currentPageIndex = Math.max(pageIndexFromUrl - 1, 0);
+
+  const totalPages = Math.ceil(data.length / pageSize);
+  const lastPageIndex = totalPages - 1;
+
+  const normalizedPageIndex = Math.min(currentPageIndex, lastPageIndex);
 
   const [pagination, setPagination] = useState({
-    pageIndex: normalizedToMinPageIndex,
-    pageSize: 10,
+    pageIndex: normalizedPageIndex,
+    pageSize,
   });
 
-  const totalPages = Math.ceil(data.length / pagination.pageSize);
-  const maxPageIndex = Math.max(0, totalPages - 1);
-
-  const normalizedToMaxPageIndex = Math.min(
-    normalizedToMinPageIndex,
-    maxPageIndex
-  );
-
   useEffect(() => {
-    if (pagination.pageIndex > maxPageIndex) {
+    if (pagination.pageIndex > lastPageIndex) {
       setPagination((prev) => ({
         ...prev,
-        pageIndex: normalizedToMaxPageIndex,
+        pageIndex: normalizedPageIndex,
       }));
       return;
     }
 
     set("page", (pagination.pageIndex + 1).toString());
   }, [
-    maxPageIndex,
-    normalizedToMaxPageIndex,
-    normalizedToMinPageIndex,
-    pageIndex,
-    pagination.pageIndex,
+    lastPageIndex,
+    normalizedPageIndex,
+    currentPageIndex,
+    pageIndexFromUrl,
+    pagination,
     set,
     totalPages,
   ]);
