d9d8c7d52a409d16ee37a28aa8a464148144e391
L1ght-time <light.time.1995@gmail.com>
2025-12-09 10:01:15 +0200

    feat: input filed component

--- views/components/fields/text-field.tsx
+++ views/components/fields/text-field.tsx
@@ -1,3 +1,46 @@
-export const TextField = () => {
-  return <div>text-field</div>;
+import { Control, FieldPath, FieldValues } from "react-hook-form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/views/components/ui/form";
+import { Input, InputProps } from "~/views/components/ui/input";
+
+export type InputFieldProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = InputProps & {
+  control: Control<TFieldValues>;
+  name: TName;
+  label?: string;
+  description?: string;
+};
+
+export const InputField = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>(
+  props: InputFieldProps<TFieldValues, TName>
+) => {
+  const { control, label, name, description, ...inputProps } = props;
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {label && <FormLabel>{label}</FormLabel>}
+          <FormControl>
+            <Input {...field} {...inputProps} />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
 };
